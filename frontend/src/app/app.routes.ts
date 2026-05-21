3d123cfa55fad9f85917e157a941102707e73c93
Dimitribonsou <dimitribonsou26@gmail.com>
2026-05-21 14:33:17 +0200

    feat(navigation): implement routing for dashboard, analysis, and shipping pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./features/dashboard/dashboard').then(d => d.Dashboard)
+  },
+  {
+    path: 'analysis',
+    loadComponent: () => import('./features/analysis/analysis').then(a => a.Analysis)
+  },
+  {
+    path: 'shipping',
+    loadComponent: () => import('./features/shipping/shipping').then(s => s.Shipping)
+  },
+];
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLinkWithHref, RouterLinkActive } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule],
+  imports: [RouterOutlet, ButtonModule, RouterLinkWithHref, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
