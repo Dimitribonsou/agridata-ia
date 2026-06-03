0f61cc69da756d3d89539e58bfab30f8e2842100
Dimitribonsou <dimitribonsou26@gmail.com>
2026-06-03 11:01:05 +0200

    feat: add CORS middleware for handling cross-origin requests

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,14 @@
 import express from "express"
 import dotnv from "dotenv";
+import cors from "cors";
 dotnv.config();
 const app: express.Application = express();
+app.use(cors(
+ {origin: "http://localhost:4200",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true}
+));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
